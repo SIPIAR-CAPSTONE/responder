14f373db8c4b828177bfc3e5001f4844f73fbdaa
verseler <handuman.verselerkerr@gmail.com>
2024-11-19 22:23:36 +0800

    feat: apply restore session after reopening the application

--- App.jsx
+++ App.jsx
@@ -5,28 +5,52 @@ import { NavigationContainer } from "@react-navigation/native";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { ToastAndroid, View } from "react-native";
 
 import { darkTheme, lightTheme } from "./utils/theme";
 import { SignedInStack, SignedOutStack } from "./navigation/StackScreen";
 import useBoundStore from "./zustand/useBoundStore";
+import useInitializeTheme from "./hooks/useInitializeTheme";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const currentThemeScheme = useBoundStore((state) => state.currentThemeScheme);
-  const setThemeScheme = useBoundStore((state) => state.setThemeScheme);
   const selectedTheme = currentThemeScheme === "light" ? lightTheme : darkTheme;
   const globalStateEncryptedSession = useBoundStore((state) => state.session);
-
+  const restoreSession = useBoundStore((state) => state.restoreSession);
+  useInitializeTheme();
 
   useEffect(() => {
-    const initThemeCheck = async () => {
-      setThemeScheme(await AsyncStorage.getItem("theme"));
-    };
-    initThemeCheck();
+    async function prepare() {
+      try {
+        await restoreSession();
+      } catch (error) {
+        ToastAndroid.show(`${error.message}`, ToastAndroid.SHORT);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   const screenOptions = {
     presentation: "containedTransparentModal",
@@ -37,16 +61,18 @@ export default function App() {
   };
 
   return (
-    <GestureHandlerRootView>
-      <BottomSheetModalProvider>
-        <PaperProvider theme={selectedTheme}>
-          <NavigationContainer>
-            <Stack.Navigator screenOptions={screenOptions}>
-              {globalStateEncryptedSession ? SignedInStack : SignedOutStack}
-            </Stack.Navigator>
-          </NavigationContainer>
-        </PaperProvider>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <GestureHandlerRootView>
+        <BottomSheetModalProvider>
+          <PaperProvider theme={selectedTheme}>
+            <NavigationContainer>
+              <Stack.Navigator screenOptions={screenOptions}>
+                {globalStateEncryptedSession ? SignedInStack : SignedOutStack}
+              </Stack.Navigator>
+            </NavigationContainer>
+          </PaperProvider>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </View>
   );
 }
